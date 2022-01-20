ce724039eaceb2a5497f154e2f2dac693ce3b1fe
conorf50 <conorf50@gmail.com>
2022-01-20 22:24:54 +0000

    rewrite based on serenity example

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,124 @@
 /*
-    A silly Discord bot which gives you facts about hay.
+    Based heavily on the Serenity examples
 
-    Based heavily on the docs here:
-    https://developers.facebook.com/blog/post/2020/09/30/build-discord-bot-with-rust-and-serenity/
+    https://github.com/serenity-rs/serenity/blob/current/examples/e14_slash_commands/src/main.rs
 
-    and the Serinity docs + examples
-    https://github.com/serenity-rs/serenity/blob/current/examples/e01_basic_ping_bot/src/main.rs
 */
+
+
 use std::env;
-use rand::prelude::*;
 use std::fs::*;
+use rand::Rng;
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready, id::UserId},
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        interactions::{
+            application_command::{
+                ApplicationCommand
+            },
+            Interaction,
+            InteractionResponseType,
+        },
+    },
     prelude::*,
-    utils::MessageBuilder,
-    model::user::*
 };
-
-struct Handler{
+struct Handler {
     fact_num: usize,
     facts_array: json::JsonValue
 }
 
-const COMMAND: &str = "/hayfacts";
-
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        //println!("{:?}", msg);
-        if msg.content == COMMAND {
-
-            let channel = match msg.channel_id.to_channel(&ctx).await {
-                Ok(channel) => channel,
-                Err(why) => {
-                    println!("Error getting channel: {:?}", why);
-                    return;
-                },
-            };
-            
-
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-
-            // Generate a random number and index into the json array object
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            // Generate a random number and get the entry at that index of the facts array
             let mut rng = rand::rngs::OsRng;
-            let rand: usize = rng.gen_range(0, self.fact_num);
+            let rand: usize = rng.gen_range(0..self.fact_num);
 
-            // build response
-            // let smart_bot_id: serenity::model::id::UserId= UserId(777199761966497842);
-        
-            
-            // let response = MessageBuilder::new()
-            // .mention(&smart_bot_id)
-            // .push("message")
-            // .build();
-
-        // if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
-        //     println!("Error sending message: {:?}", why);
-        // }
             let response =  format!("{}", self.facts_array["facts"][rand]);
-           
-            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
-                println!("Error sending message: {:?}", why);
+
+            let content = match command.data.name.as_str() {
+                "hayfacts" => response,
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
             }
-
-            // 777199761966497842
-
-
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command.name("hayfacts").description("Facts about hay.")
+                })
+        })
+        .await;
+
+        println!("I now have the following guild slash commands: {:#?}", commands);
+
+        let guild_command =
+            ApplicationCommand::create_global_application_command(&ctx.http, |command| {
+                command.name("hayfacts").description("Facts about hay.")
+            })
+            .await;
+
+        println!("I created the following global slash command: {:#?}", guild_command);
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main(){
     // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_SECRET_TOKEN")
-        .expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-            // Read the local JSON file containing those juicy facts
-            // TODO, fix error handling
-            let fstr = read_to_string("data/hayfacts.json").unwrap();
-            let parsed = json::parse(&fstr).unwrap();
+    // Read the local JSON file containing those juicy facts
+    // TODO, fix error handling
+    let fstr = read_to_string("data/hayfacts.json").unwrap();
+    let parsed = json::parse(&fstr).unwrap();
 
-            // Calculate how many facts we have so the rng won't generate values outside this
-            // range
-            let facts_array_len = parsed["facts"].len(); // access the 'facts' sub array
+    // Calculate how many facts we have so the rng won't generate values outside this
+    // range
+    let facts_array_len = parsed["facts"].len(); // access the 'facts' sub array
 
+    // The Application Id is usually the Bot User Id.
+    let application_id: u64 = env::var("APPLICATION_ID")
+        .expect("Expected an application id in the environment")
+        .parse()
+        .expect("application id is not a valid id");
 
-            // Create a new instance of the Client, logging in as a bot. This will
-            // automatically prepend your bot token with "Bot ", which is a requirement
-            // by Discord for bot users.
-            let mut client = Client::builder(&token)
-                .event_handler(Handler {
-                    facts_array: parsed,
-                    fact_num: facts_array_len
-                })
-                .await
-                .expect("Err creating client");
+    // Build our client.
+    let mut client = Client::builder(token)
+        .event_handler(Handler{fact_num: facts_array_len, facts_array: parsed })
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
 
-            // Finally, start a single shard, and start listening to events.
-            //
-            // Shards will automatically attempt to reconnect, and will perform
-            // exponential backoff until it reconnects.
-            if let Err(why) = client.start().await {
-                println!("Client error: {:?}", why);
-            }
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
